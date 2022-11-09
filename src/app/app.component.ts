76dfd23ab9f38998cec35cf6b0a97b960b5d23fb
Raji G <rajith0417@gmail.com>
2022-11-09 21:06:40 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trackBy';
+  list = [{id: 1}, {id: 2}, {id: 3}, {id: 4}];
+
+  getItems() {
+    this.list = [{id: 1}, {id: 2}, {id: 3}, {id: 5}];
+  }
+
+  trackByFunction(index: number, name: any): number {
+    return name.id;
+  }
+
+
 }
