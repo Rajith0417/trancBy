5200a7e22abee25e07c23046283408455081a23b
Raji G <rajith0417@gmail.com>
2023-02-22 00:35:18 +0530

    modified to object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface details{
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +13,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'trackBy';
   list = [{id: 1}, {id: 2}, {id: 3}, {id: 4}];
+  list2 = [{id: 1, name: "one"}, {id: 2, name: "two"}, {id: 3, name: "three"}, {id: 4, name: "four"}];
 
   getItems() {
     this.list = [{id: 1}, {id: 2}, {id: 3}, {id: 5}];
+    this.list2 = [{id: 1, name: "onees"}, {id: 2, name: "two"}, {id: 3, name: "three"}, {id: 5, name: "five"}];
   }
 
   trackByFunction(index: number, name: any): number {
     return name.id;
+  }
+
+  trackByFunction2(index: number, details: any): number {
+    return details.id;
   }
 
 }
